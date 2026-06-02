d277877b0244ee0fd6658716b40f65d52b61b065
Vishwanath <thevishwa01@gmail.com>
2026-06-02 12:08:36 +0530

    Practice

---
+++ src/LoopPractice/Factorial.java
@@ -0,0 +1,22 @@
+package LoopPractice;
+
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String[] args) {
+
+     int fact=1;
+     
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     
+     
+     for(int i=1;i<=n;i++) {
+    	 
+    	 fact*=i;
+    	 
+     }
+     System.out.println(fact);
+	
+	}
+}
