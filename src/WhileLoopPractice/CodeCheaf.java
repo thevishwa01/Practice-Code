2a305c2ae9222c33673a38a144bb86b557101514
Vishwanath <thevishwa01@gmail.com>
2026-06-04 13:07:32 +0530

    Code Cheaf

---
+++ src/WhileLoopPractice/CodeCheaf.java
@@ -0,0 +1,21 @@
+package WhileLoopPractice;
+
+import java.util.Scanner;
+
+public class CodeCheaf {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+
+		for (int i = 0; i < t; i++) {
+
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			int d = sc.nextInt();
+			int e = sc.nextInt();
+			System.out.println(a + " " + b + " " + c + " " + d + " " + e);
+		}
+	}
+}
