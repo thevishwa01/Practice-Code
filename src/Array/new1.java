e4330e0922f94ab2bf3b58950c0406872fd60ac2
Vishwanath <thevishwa01@gmail.com>
2026-06-29 10:23:58 +0530

    Arrya Practice

---
+++ src/Array/new1.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class new1 {
+
+	public static void main(String[] args) {
+
+		int[] a = { 1, 23, 55, 23, 664, 74 };
+
+		int max = a[0];
+
+		for (int i = 1; i < a.length; i++) {
+
+			if (a[i] > max) {
+
+				max = a[i];
+
+			}
+
+		}
+		System.out.println(max);
+
+	}
+
+}
